8c4fe62aed94fece1ca5e2236b1af671e8c32de9
Adarsh Hiwrale <ahiwrale@sigmasolve.net>
2024-03-13 03:40:37 +0530

    sid

--- iam.tf
+++ iam.tf
@@ -9,6 +9,7 @@ data "aws_iam_policy_document" "assume_role" {
   count = local.create_role ? 1 : 0
 
   statement {
+    sid     = "AllowStepFunctionsAssumeRole"
     effect  = "Allow"
     actions = ["sts:AssumeRole"]
 
